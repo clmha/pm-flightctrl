164f1f970aab8c6ef52e59222007614e0a65297b
clmha <45580177+clmha@users.noreply.github.com>
2022-06-26 21:50:16 -0400

    F104 - Defines Flight Conditions

---
+++ f104/F104_latDirAnalysis.m
@@ -0,0 +1,11 @@
+%% Lateral-Directional Handling Qualities Design for the Lockheed F-104 Starfighter Aircraft
+%
+% Source: "Flight Dynamics Principles" by Michael V. Cook, Assignment 3.
+
+%% Aircraft Model
+alt = [0 0 0]; % (ft)
+airDensity = [0.00238 0.00238 0.00238]; % (slug/ft^3)
+speedOfSound = [1116.44 1116.44 1116.44]; % (ft/s)
+gAccel = [32.2 32.2 32.2]; % (ft/s^2)
+trimMach = [0.257 0.8 1.1];
+trimPitchAttitude = [2.30 2 1]; % (°)
